a78f3dfbd113573dcb6e1f103ea461293ec2d509
Michael Dance <michael.dance@agencychief.com>
2018-01-08 13:49:34 -0500

    Test sending/receiving attachments

--- index.js
+++ index.js
@@ -97,6 +97,36 @@ function handleMessage(sender_psid, received_message) {
       'text': `You sent the message: "${received_message.text}". Now send me an image!`
     }
 
+  } else if (received_message.attachments) {
+
+    // Get the URL of the first message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      'attachment': {
+        'type':'template',
+        'payload': {
+          'template_type': 'generic',
+          'elements': [{
+            'title': 'Is this the right picture?',
+            'subtitle': 'Tap a button to answer.',
+            'image_url': attachment_url,
+            'buttons': [
+              {
+                'type': 'postback',
+                'title': 'Yes!',
+                'payload': 'yes'
+              },
+              {
+                'type': 'postback',
+                'title': 'No way.',
+                'payload': 'no'
+              }
+            ]
+          }]
+        }
+      }
+    }
+
   }
 
   // Sends the response message
