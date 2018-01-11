9c2e25bb381100e36190310a918007cff7252fe8
Michael Dance <michael.dance@agencychief.com>
2018-01-11 09:52:20 -0500

    Add console logs for testing

--- index.js
+++ index.js
@@ -1,70 +1,66 @@
-// Step 2
-
 'use strict';
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
 
-// Imports dependencies and set up http server
+// Import dependencies and set up http server
 const
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  request = require('request'),
-  app = express().use(bodyParser.json()); // creates express http server
+  express           = require('express'),
+  bodyParser        = require('body-parser'),
+  app               = express().use(bodyParser.json()), // creates express http server
+  request           = require('request'),
+  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-// Sets server port and logs message on success
+// Set server port and logs message on success
 app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
 
-// Creates the endpoint for our webhook
+/**
+ * Create webhook endpoint for POST requests: receive messages and funnel
+ * them to the appropriate function.
+ */
 app.post('/webhook', (req, res) => {
 
   let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check this is an event from a FB page
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // entry.messaging is array, but will only ever contain one message
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-
-      // Check if the event is a message or postback and pass the
-      // event to the appropriate handler function
+      // Determine if event is a message or a postback
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
+        handleMessage(webhook_event.sender.id, webhook_event.message);
       } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback);
+        handlePostback(webhook_event.sender.id, webhook_event.postback);
       }
 
     });
 
-    // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
 });
 
-// Adds support for GET requests to our webhook
+/**
+ * Create webhook endpoint for GET requests. This is used specifically to
+ * verify the webhook when setting up the app.
+ */
 app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string
   const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
+  let mode      = req.query['hub.mode'];
+  let token     = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
   // Checks if a token and mode is in the query string of the request
@@ -78,19 +74,23 @@ app.get('/webhook', (req, res) => {
       res.status(200).send(challenge);
 
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
+
   }
 });
 
-// Handles messages events
+/**
+ * Handle message events.
+ */
 function handleMessage(sender_psid, received_message) {
 
   let response;
 
   // Check if the message contains text
   if ( received_message.text ) {
+
+    console.log( received_message );
 
     // Create the payload for a basic text message
     response = {
@@ -134,10 +134,14 @@ function handleMessage(sender_psid, received_message) {
 
 }
 
-// Handles messaging_postbacks events
+/**
+ * Handle postback events.
+ */
 function handlePostback(sender_psid, received_postback) {
 
   let response;
+
+  console.log( received_postback );
 
   // Get the payload for the postback
   let payload = received_postback.payload;
@@ -152,7 +156,9 @@ function handlePostback(sender_psid, received_postback) {
 
 }
 
-// Sends response messages via the Send API
+/**
+ * Send response messages via the Send API.
+ */
 function callSendAPI(sender_psid, response) {
 
   // Construct the message body
