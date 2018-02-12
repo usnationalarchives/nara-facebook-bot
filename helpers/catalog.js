0e3c4d0b18c5a17981ac4b620c006c06177cd986
Michael Dance <michael.dance@agencychief.com>
2018-02-12 16:56:21 -0500

    Improve logic to get random array item

--- helpers/catalog.js
+++ helpers/catalog.js
@@ -41,131 +41,114 @@ const getItem = ( user, tagRoundCount = 0, startMessage = '' ) => {
 		.then( function( res ) {
 			console.log( 'Request response', res );
 
-			// the api has been known to return empty results for unclear reasons
-			if ( ! res.data.opaResponse.results.result.length ) {
+			let result = res.data.opaResponse.results.result[0];
+			let objects = result.objects.object;
 
-				// attempt to try again
-				getItem( user, tagRoundCount, startMessage );
-
-			} else {
-
-				let result = res.data.opaResponse.results.result[0];
-				let objects = result.objects.object;
-
-				// ensure objects is an array
-				if ( ! Array.isArray( objects ) ) {
-					objects = [ objects ];
-				}
-
-				let thisObject;
-
-				// get one specific object
-				if ( objects.length > 1 ) {
-					// choose at random if more than one
-					let objNum = Math.floor( Math.random() * objects.length ) + 1;
-					thisObject = objects[objNum];
-				} else {
-					thisObject = objects[0];
-				}
-
-				let naId = result.description.item.naId;
-
-				let newTagRoundCount = tagRoundCount + 1;
-
-				// send title, with follow-up catalog object and answer prompt
-				sendApi.sendMessage( user, result.description.item.title + ':', [
-					{
-						'attachment': {
-							'type': 'template',
-							'payload': {
-								'template_type': 'generic',
-								'sharable': true,
-								'image_aspect_ratio': 'square',
-								'elements': [
-									{
-										'title': result.description.item.title,
-										'image_url': thisObject.file['@url'],
-										'default_action': {
-											'type': 'web_url',
-											'url': thisObject.file['@url']
-										},
-										'buttons': [
-											{
-												'type': 'web_url',
-												'url': thisObject.file['@url'],
-												'title': script.tag_image_options.big,
-											},
-											{
-												'type': 'web_url',
-												'url': 'https://catalog.archives.gov/id/' + naId,
-												'title': script.tag_image_options.learn,
-												'webview_height_ratio': 'tall',
-												'messenger_extensions': true
-											}
-										]
-									}
-								]
-							}
-						}
-					},
-					{
-						'text': script.tag_prompt.message,
-						'quick_replies': [
-							{
-								'content_type': 'text',
-								'title': script.tag_prompt.options.handwritten,
-								'payload': JSON.stringify( {
-									'name': 'tag.options.handwritten',
-									'type': 'JSON',
-									'tag_round_count': newTagRoundCount,
-									'naId':naId
-								} )
-							},
-							{
-								'content_type': 'text',
-								'title': script.tag_prompt.options.typed,
-								'payload': JSON.stringify( {
-									'name': 'tag.options.typed',
-									'type': 'JSON',
-									'tag_round_count': newTagRoundCount,
-									'naId':naId
-								} )
-							},
-							{
-								'content_type': 'text',
-								'title': script.tag_prompt.options.mixed,
-								'payload': JSON.stringify( {
-									'name': 'tag.options.mixed',
-									'type': 'JSON',
-									'tag_round_count': newTagRoundCount,
-									'naId':naId
-								} )
-							},
-							{
-								'content_type': 'text',
-								'title': script.tag_prompt.options.none,
-								'payload': JSON.stringify( {
-									'name': 'tag.options.none',
-									'type': 'JSON',
-									'tag_round_count': newTagRoundCount,
-									'naId':naId
-								} )
-							},
-							{
-								'content_type': 'text',
-								'title': script.tag_prompt.options.skip,
-								'payload': JSON.stringify( {
-									'name': 'tag.options.skip',
-									'type': 'JSON',
-									'tag_round_count': tagRoundCount, // don't increment skipped items
-									'naId':naId
-								} )
-							}
-						]
-					}
-				] );
-
+			// ensure objects is an array
+			if ( ! Array.isArray( objects ) ) {
+				objects = [ objects ];
 			}
+
+			// choose an array item at random
+			let objNum = Math.floor( Math.random() * objects.length );
+			let thisObject = objects[objNum];
+
+			let naId = result.description.item.naId;
+
+			let newTagRoundCount = tagRoundCount + 1;
+
+			// send title, with follow-up catalog object and answer prompt
+			sendApi.sendMessage( user, result.description.item.title + ':', [
+				{
+					'attachment': {
+						'type': 'template',
+						'payload': {
+							'template_type': 'generic',
+							'sharable': true,
+							'image_aspect_ratio': 'square',
+							'elements': [
+								{
+									'title': result.description.item.title,
+									'image_url': thisObject.file['@url'],
+									'default_action': {
+										'type': 'web_url',
+										'url': thisObject.file['@url']
+									},
+									'buttons': [
+										{
+											'type': 'web_url',
+											'url': thisObject.file['@url'],
+											'title': script.tag_image_options.big,
+										},
+										{
+											'type': 'web_url',
+											'url': 'https://catalog.archives.gov/id/' + naId,
+											'title': script.tag_image_options.learn,
+											'webview_height_ratio': 'tall',
+											'messenger_extensions': true
+										}
+									]
+								}
+							]
+						}
+					}
+				},
+				{
+					'text': script.tag_prompt.message,
+					'quick_replies': [
+						{
+							'content_type': 'text',
+							'title': script.tag_prompt.options.handwritten,
+							'payload': JSON.stringify( {
+								'name': 'tag.options.handwritten',
+								'type': 'JSON',
+								'tag_round_count': newTagRoundCount,
+								'naId':naId
+							} )
+						},
+						{
+							'content_type': 'text',
+							'title': script.tag_prompt.options.typed,
+							'payload': JSON.stringify( {
+								'name': 'tag.options.typed',
+								'type': 'JSON',
+								'tag_round_count': newTagRoundCount,
+								'naId':naId
+							} )
+						},
+						{
+							'content_type': 'text',
+							'title': script.tag_prompt.options.mixed,
+							'payload': JSON.stringify( {
+								'name': 'tag.options.mixed',
+								'type': 'JSON',
+								'tag_round_count': newTagRoundCount,
+								'naId':naId
+							} )
+						},
+						{
+							'content_type': 'text',
+							'title': script.tag_prompt.options.none,
+							'payload': JSON.stringify( {
+								'name': 'tag.options.none',
+								'type': 'JSON',
+								'tag_round_count': newTagRoundCount,
+								'naId':naId
+							} )
+						},
+						{
+							'content_type': 'text',
+							'title': script.tag_prompt.options.skip,
+							'payload': JSON.stringify( {
+								'name': 'tag.options.skip',
+								'type': 'JSON',
+								'tag_round_count': tagRoundCount, // don't increment skipped items
+								'naId':naId
+							} )
+						}
+					]
+				}
+			] );
 
 		} )
 		.catch( function( error ) {
