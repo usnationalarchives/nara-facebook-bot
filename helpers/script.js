4a80586234f5852d9a9ff5240810748bac838a89
Michael Dance <michael.dance@agencychief.com>
2018-02-23 11:31:26 -0500

    Add fun facts

--- helpers/script.js
+++ helpers/script.js
@@ -120,14 +120,23 @@ const script = {
 	// Fun Facts
 	//
 	'facts': [
-		{
-			'message': 'In true pop culture fashion, the most requested photo from the National Archives is of Nixon and The King.',
-			'image': 'https://catalog.archives.gov/OpaAPI/media/1634221/content/arcmedia/Public_Vaults/5598_2004_a.jpg'
-		},
-		'The National Archives Building in downtown Washington, DC is built above an underground stream, the Old Tiber Creek.',
-		'The National Archives was established in 1934 by President Franklin Roosevelt, but you could say it started with the birth of our great nation. Our documents date back to 1774.',
-		'We\'re more than the Declaration of Independence (my eye is on you, Nicolas Cage wannabe). In Washington, DC, there are just about 10 billion (BILLION) pieces of paper alone. To read that many pages, you\'d have to read the entire Harry Potter series 3,197,953 times!',
-		'You can be a virtual volunteer! Become a Citizen Archivist by transcribing and tagging records in our Catalog.',
+		'The National Archives Building in downtown Washington, DC, is built above an underground stream, the Old Tiber Creek.',
+		'The National Archives was established in 1934 by President Franklin Roosevelt. Before that, records were kept by the agencies and sometimes even in garages, attics, and basements of Federal buildings! https://www.archives.gov/about/history',
+		'You can be a virtual volunteer! Become a Citizen Archivist by transcribing and tagging records in our Catalog. https://www.archives.gov/citizen-archivist',
+		'The National Archives is known as the nation\'s record keeper. Of all the documents and materials created in the course of business conducted by the United States Federal government, only 1%-3% are so important for legal or historical reasons that they are kept by us forever. And that still amounts to 130 billion pages!',
+		'The most downloaded image from our Catalog is the Engrossed Declaration of Independence: https://catalog.archives.gov/id/1419123 Explore and download more from our digital holdings at https//catalog.archives.gov',
+		'There are approximately 13 billion pages of textual records at the National Archives, but we\'re more than just paper. We also house 12 million maps, charts, and architectural and engineering drawings; 25 million still photographs and graphics; 24 million aerial photographs; 300,000 reels of motion picture film; 400,000 video and sound recordings; and 133 terabytes of electronic data.',
+		'The Rotunda of the National Archives Museum in Washington, DC, holds the Declaration of Independence, the Constitution, and the Bill of Rights.',
+		'Get your Presidential Libraries passport and take a roadtrip across the country. Each Presidential Library has its own unique commemorative stamp. After collecting all of them, you can collect a special gift from one of the museum stores.',
+		'We celebrate Independence Day with a special dramatic reading of the Declaration of Independence. Join us on July 4 for some patriotic festivities on the steps of the National Archives Building -- it\'s our most popular event of the year!',
+		'Did you know that admission to the National Archives Museum is always free? Start planning your visit today! https://www.archives.gov/museum/',
+		'The Declaration of Independence is housed in a sealed encasement filled with the inert gas argon and with controlled temperature and humidity to keep the parchment flexible.',
+		'The Constitution was not America\'s first constitution. The Articles of Confederation was the plan of government in effect from 1781 to 1787. It was based on the principles of the Declaration of Independence, but specified no central powers over taxation and trade, nor the ability to compel the states to fulfill national obligations.',
+		'The earliest sound recordings were made on wax cylinders. You can hear a rare recording of Theodore Roosevelt speaking in the Public Vaults exhibition at the National Archives Museum in Washington, DC. https://www.archives.gov/museum/visit/vaults.html',
+		'You might want to bring a sweater when you visit the Declaration, Constitution, and Bill of Rights. Thermostats and light levels in the Rotunda are deliberately kept low because cooler temperatures prolong the life of documents and light fades ink and destroys parchment and paper.',
+		'The National Archives Building was designed by architect John Russell Pope as a temple to history. Pope also designed the Jefferson Memorial; do you see any similarities? https://catalog.archives.gov/id/35810272 https://catalog.archives.gov/id/7873491',
+		'The National Archives Building stands at 166 feet tall, 213 feet wide, and 330 feet long.',
+		'At the top of grand staircase of the National Archives Building rest two giant bronze doors leading to the Rotunda. Each door weighs 6.5 tons and is 38.7 feet high, 10 feet wide, and 11 inches thick. But don\'t worry about holding the door open for the next person in line; these doors no longer function as the main entrance to the National Archives and are rarely opened (but continue to be an impressive feature of the building!).'
 	],
 	'facts_reply': {
 		'message': 'Want another?',
